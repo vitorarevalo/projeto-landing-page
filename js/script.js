fa7f2e43adc7d850b2ed9f3b8c96cb580ebe7390
vitorarevalo <vitinhoosuna2008@gmail.com>
2024-02-13 20:32:26 -0300

    menu pronto, deu muito trabalho

--- js/script.js
+++ js/script.js
@@ -3,17 +3,19 @@ var menu = document.querySelector("#itens");
 var pai = burguer.parentNode;
 
 var closer = document.querySelector("#closer")
-
+var body = document.querySelector("body")
 
 
 
 
 burguer.addEventListener("click", function mostrarElementos(){
-    menu.style.cssText = "display: flex;"
+    menu.style.cssText = "display: flex; transition: 0.5s"
     closer.style.display = "block"
+
 })
 
-
-closer.addEventListener("click", function esconderElementos(){
+    closer.addEventListener("click", function esconderElementos(){
     menu.style.display = "none"
+    
 })
+
